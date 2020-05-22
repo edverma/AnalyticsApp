cecbebd8eaad3826b66673f83c08545ed8607e78
edverma <edv24019@uga.edu>
2020-05-22 14:04:27 -0400

    added pie chart with percentages

--- backend/analytics/analytics.py
+++ backend/analytics/analytics.py
@@ -1,6 +1,6 @@
 # TODO: create mongodb connection and retrieve all entries for user
 
-import sys, json
+import sys, json, re
 import matplotlib.pyplot as plt
 
 jsonInfo = ''
@@ -9,6 +9,32 @@ for line in sys.stdin:
 	break
 jsonInfo = json.loads(jsonInfo)
 
-jsonString = json.dumps(jsonInfo)
+domainCounts = dict()
+totalCount = 0
+for record in jsonInfo:
+    endDomainIndex = -1
+    try:
+        endDomainIndex = record['url'].index('.com')
+    except ValueError:
+        try:
+            endDomainIndex = record['url'].index('.org')
+        except ValueError:
+            try:
+                endDomainIndex = record['url'].index('.net')
+            except ValueError:
+                continue
+        
+    endDomainIndex += 4
+    totalCount += 1
+    domain = record['url'][0:endDomainIndex]
+    if domain not in domainCounts:
+        domainCounts[domain] = 1
+    else:
+        domainCounts[domain] += 1
+        
+for key in domainCounts.keys():
+    domainCounts[key] /= totalCount
+
+jsonString = json.dumps(domainCounts)
 
 print(jsonString)
